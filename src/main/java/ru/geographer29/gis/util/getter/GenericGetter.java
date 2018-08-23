0baea6e64108810afeebda3f17129ed83b75c030
i.krasnikov <geographer29@yandex.ru>
2018-08-23 12:06:14 +0300

    Generic Getter is package-private now

--- src/main/java/ru/geographer29/gis/util/getter/GenericGetter.java
+++ src/main/java/ru/geographer29/gis/util/getter/GenericGetter.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Field;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class GenericGetter<T> implements Gettable<T>{
+class GenericGetter<T> implements Gettable<T>{
     private static final Logger logger = LogManager.getLogger();
 
     @SuppressWarnings("unchecked")
@@ -16,7 +16,7 @@ public class GenericGetter<T> implements Gettable<T>{
             Field field = targetClass.getClass().getDeclaredField(fieldName);
             obj = (T)field.get(targetClass);
         } catch (NoSuchFieldException | IllegalAccessException e) {
-            logger.fatal("Getter is not working!");
+            logger.fatal("Generic getter is not working!");
         }
 
         return obj;
