33f75eb279d5f8dae56c3808f8d28e36f43c290a
i.krasnikov <geographer29@yandex.ru>
2018-08-23 17:52:48 +0300

    Import class now supports import of layers with different extentions

--- src/main/java/ru/geographer29/gis/model/logic/imports/Import.java
+++ src/main/java/ru/geographer29/gis/model/logic/imports/Import.java
@@ -1,22 +1,21 @@
-package ru.geographer29.gis.model.logic;
+package ru.geographer29.gis.model.logic.imports;
 
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.view.MapView;
-import javafx.collections.ObservableList;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.geographer29.gis.config.DefaultConfig;
 import ru.geographer29.gis.config.FileExtension;
-import ru.geographer29.gis.controllers.MainController;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
-public class Import extends MainController {
+public class Import{
     private MapView mapView;
+    private static final Logger logger = LogManager.getLogger();
 
     public void importFiles(MapView map){
         mapView = map;
@@ -25,19 +24,22 @@ public class Import extends MainController {
         chooser.getExtensionFilters().addAll(FileExtension.filters);
         chooser.setInitialDirectory(DefaultConfig.INITIAL_FOLDER);
         List<File> files = chooser.showOpenMultipleDialog((Stage)mapView.getScene().getWindow());
-        if (files != null) addLayers(files);
-    }
 
-    private void addLayers(List<File> list){
-        ObservableList<Layer> obsList  = getLayersTable();
-        for (File file: list ){
-            Layer layer = new Layer(file);
-            getExtent(layer.getFeatureLayer());
-            mapView.getMap().getOperationalLayers().add(layer.getFeatureLayer());
-            obsList.add(layer);
+        if (files != null){
+            for (File file: files ){
+                ImportHandler importHandler = new ImportHandler(file);
+
+                switch (getFileExtension(file)){
+                    case "shp" : importHandler.importWith(new ShapefileImport());
+                    case "tiff" : importHandler.importWith(new TiffImport());
+                    case "tif" : importHandler.importWith(new TiffImport());
+                    default : {
+                        throw new RuntimeException("Incorrect file extension...");
+                        logger.error("Incorrect file extension...");
+                    }
+                }
+            }
         }
-        System.out.println(obsList + "  obs list");
-        getTable().setItems(obsList);
     }
 
     private void getExtent(FeatureLayer layer){
@@ -55,11 +57,8 @@ public class Import extends MainController {
 
     private static String getFileExtension(File file) {
         String fileName = file.getName();
-        // если в имени файла есть точка и она не является первым символом в названии файла
         if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
-            // то вырезаем все знаки после последней точки в названии файла, то есть ХХХХХ.txt -> txt
             return fileName.substring(fileName.lastIndexOf(".") + 1);
-            // в противном случае возвращаем заглушку, то есть расширение не найдено
         else return "";
     }
 
